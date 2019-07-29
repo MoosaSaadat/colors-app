aecbbad48f1d5946ece3820da839b30a9fe2d4d6
Muhammad Musa <moosasaadat99@gmail.com>
2019-07-29 20:50:27 +0500

    Updates

---
+++ precache-manifest.8cbc7572c91399bc00ff4a393d49c965.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7847eab0c81f7cb8cd4326a1565805d",
+    "url": "/colors-app/index.html"
+  },
+  {
+    "revision": "1799b73be7eaa87b0e21",
+    "url": "/colors-app/static/css/2.e8e75e1a.chunk.css"
+  },
+  {
+    "revision": "cc58d592710dfddbe2a4",
+    "url": "/colors-app/static/css/main.5e9766f0.chunk.css"
+  },
+  {
+    "revision": "1799b73be7eaa87b0e21",
+    "url": "/colors-app/static/js/2.ccab0bc3.chunk.js"
+  },
+  {
+    "revision": "cc58d592710dfddbe2a4",
+    "url": "/colors-app/static/js/main.411e142b.chunk.js"
+  },
+  {
+    "revision": "658f8f8a04f3c7d8f86e",
+    "url": "/colors-app/static/js/runtime~main.7b7bc380.js"
+  }
+]);
