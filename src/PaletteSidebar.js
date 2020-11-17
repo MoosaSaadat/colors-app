897d7dabd4a608da508b289a36fda02351535ae2
Muhammad Musa <moosasaadat99@gmail.com>
2020-11-17 22:16:18 +0500

    Resolve 'boolean' prop error

--- src/PaletteSidebar.js
+++ src/PaletteSidebar.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
 import Snackbar from "@material-ui/core/Snackbar";
@@ -8,101 +8,108 @@ import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import { withStyles } from "@material-ui/styles";
 import { Link } from "react-router-dom";
 // import Slider from "rc-slider";
-import Logo from "./imgs/new_logo.png"
+import Logo from "./imgs/new_logo.png";
 import styles from "./styles/PaletteSidebarStyles";
 import "rc-slider/assets/index.css";
-import { FormControl, FormLabel, InputLabel, Slider, Typography } from '@material-ui/core';
+import {
+  FormControl,
+  FormLabel,
+  InputLabel,
+  Slider,
+  Typography,
+} from "@material-ui/core";
 
 class PaletteSidebar extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-		this.state = {
-			format: "hex",
-			open: false
-		};
-		this.handleFormatChange = this.handleFormatChange.bind(this);
-		this.closeSnackbar = this.closeSnackbar.bind(this);
+    this.state = {
+      format: "hex",
+      open: false,
+    };
+    this.handleFormatChange = this.handleFormatChange.bind(this);
+    this.closeSnackbar = this.closeSnackbar.bind(this);
   }
-	handleFormatChange (e) {
-		this.setState({ format: e.target.value, open: true });
-		this.props.changeFormat(e.target.value);
-	}
-	closeSnackbar () {
-		this.setState({ open: false });
-	}
-  render (){
-		const { level, changeLevel, showSlider, classes } = this.props;
+  handleFormatChange(e) {
+    this.setState({ format: e.target.value, open: true });
+    this.props.changeFormat(e.target.value);
+  }
+  closeSnackbar() {
+    this.setState({ open: false });
+  }
+  render() {
+    const { level, changeLevel, showSlider, classes } = this.props;
     const { format, open } = this.state;
 
     return (
       <nav className={classes.Sidebar}>
+        <img
+          className={classes.SidebarLogo}
+          src={Logo}
+          alt="Color Palettes Logo"
+        />
 
-        <img className={classes.SidebarLogo} src={Logo} alt="Color Palettes Logo" />
+        <FormControl className={classes.SelectContainer}>
+          <FormLabel component="legend" id="format-label">
+            Code Format:
+          </FormLabel>
+          <Select
+            aria-labelledby="format-label"
+            className={classes.SelectField}
+            value={format}
+            onChange={this.handleFormatChange}>
+            <MenuItem value="hex">HEX</MenuItem>
+            <MenuItem value="rgb">RGB</MenuItem>
+            <MenuItem value="rgba">RGBA</MenuItem>
+          </Select>
+        </FormControl>
 
-				<FormControl className={classes.SelectContainer}>
-					<FormLabel component="legend" id="format-label">
-						Code Format:
-					</FormLabel>
-					<Select
-						aria-labelledby="format-label"
-						className={classes.SelectField}
-						value={format}
-						onChange={this.handleFormatChange}
-					>
-						<MenuItem value="hex">HEX</MenuItem>
-						<MenuItem value="rgb">RGB</MenuItem>
-						<MenuItem value="rgba">RGBA</MenuItem>
-					</Select>
-				</FormControl>
+        {showSlider && (
+          <div className={classes.Slider}>
+            <Slider
+              orientation="vertical"
+              defaultValue={level}
+              min={100}
+              max={900}
+              step={100}
+              onChange={changeLevel}
+              valueLabelDisplay="auto"
+              classes={{
+                track: classes.SliderTrack,
+                thumb: classes.SliderThumb,
+                rail: classes.SliderRail,
+              }}
+            />
+            <Typography gutterBottom className={classes.SliderLabel}>
+              Shade: {level}
+            </Typography>
+          </div>
+        )}
 
-				{showSlider && (
-					<div className={classes.Slider}>
-						<Slider
-							orientation="vertical"
-							defaultValue={level}
-							min={100}
-							max={900}
-							step={100}
-							track={false}
-							onChange={changeLevel}
-							valueLabelDisplay="auto"
-							classes={{
-								track: classes.SliderTrack,
-								thumb: classes.SliderThumb,
-								rail: classes.SliderRail,
-							}}
-		        />
-						<Typography gutterBottom className={classes.SliderLabel}>
-							Shade: {level}
-						</Typography>
-					</div>
-				)}
-
-				<Snackbar
-					anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-					open={open}
-					autoHideDuration={3000}
-					message={
-						<span id="message-id">
-							Format Changed to {format.toUpperCase()}
-						</span>
-					}
-					ContentProps={{
-						"aria-describedby": "message-id"
-					}}
-					onClose={this.closeSnackbar}
-					action={[
-						<IconButton
-							onClick={this.closeSnackbar}
-							color="inherit"
-							key="close"
-							aria-label="close">
-							<CloseIcon />
-						</IconButton>
-					]}
-				/>
+        <Snackbar
+          anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+          open={open}
+          autoHideDuration={3000}
+          message={
+            <span id="message-id">
+              Format Changed to {format.toUpperCase()}
+            </span>
+          }
+          ContentProps={{
+            "aria-describedby": "message-id",
+          }}
+          onClose={this.closeSnackbar}
+          action={[
+            <IconButton
+              onClick={this.closeSnackbar}
+              color="inherit"
+              key="close"
+              aria-label="close">
+              <CloseIcon />
+            </IconButton>,
+          ]}
+        />
       </nav>
-    )
+    );
   }
 }
 
