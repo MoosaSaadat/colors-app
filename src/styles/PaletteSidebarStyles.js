f6828404050d2371062a3f0eebf8cb1415cbc9a5
Muhammad Musa <moosasaadat99@gmail.com>
2020-12-13 21:11:19 +0500

    Fix copy animation

--- src/styles/PaletteSidebarStyles.js
+++ src/styles/PaletteSidebarStyles.js
@@ -1,57 +1,57 @@
 import sizes from "../size";
 
 export default {
-	Sidebar: {
-		position: "fixed",
-		top: "0",
-		left: "0",
+  Sidebar: {
+    position: "fixed",
+    top: "0",
+    left: "0",
     display: "flex",
     flexFlow: "column wrap",
-		alignItems: "center",
-		justifyContent: "flex-start",
-		height: "100vh",
-		width: "15vw",
-		zIndex: "10",
-		backgroundColor: "white",
-		boxShadow:
-			"0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12)"
+    alignItems: "center",
+    justifyContent: "flex-start",
+    height: "100vh",
+    width: "15vw",
+    zIndex: "10",
+    backgroundColor: "white",
+    boxShadow:
+      "0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12)",
   },
   SidebarLogo: {
     maxWidth: "100%",
     maxHeight: "auto",
-		marginBottom: "1rem",
-		boxSizing: "border-box"
+    marginBottom: "1rem",
+    boxSizing: "border-box",
   },
-	SelectContainer: {
-		width: "85%",
-		marginBottom: "1rem",
-		flexFlow: "row nowrap",
-		alignItems: "center",
-		justifyContent: "space-between",
-	},
-	SelectField: {
-		fontSize: "14px"
-	},
-	Slider: {
-		height: "40vh",
-		marginBottom: "1rem",
-		display: "flex",
-		flexFlow: "column",
-		alignItems: "center"
-	},
-	SliderTrack: {
-		display: "none"
-	},
-	SliderRail: {
-		width: "5px !important",
-	},
-	SliderThumb: {
-		width: "15px",
-		height: "15px",
-	},
-	SliderLabel: {
-		fontSize: "1rem",
-		color: "rgba(0, 0, 0, 0.54)",
-		marginTop: "2rem",
-	},
+  SelectContainer: {
+    width: "85%",
+    marginBottom: "1rem",
+    flexFlow: "row nowrap",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  SelectField: {
+    fontSize: "14px",
+  },
+  Slider: {
+    height: "40vh",
+    marginBottom: "1rem",
+    display: "flex",
+    flexFlow: "column",
+    alignItems: "center",
+  },
+  SliderTrack: {
+    display: "none",
+  },
+  SliderRail: {
+    width: "5px !important",
+  },
+  SliderThumb: {
+    width: "15px",
+    height: "15px",
+  },
+  SliderLabel: {
+    fontSize: "1rem",
+    color: "rgba(0, 0, 0, 0.54)",
+    marginTop: "2rem",
+  },
 };
--- src/styles/PaletteStyles.js
+++ src/styles/PaletteStyles.js
@@ -1,13 +1,13 @@
 export default {
-	Palette: {
-		height: "93vh",
-		overflow: "scroll",
-		marginTop: "7vh",
-		marginLeft: "15vw",
-		display: "flex",
-		flexFlow: "row wrap",
-		alignItems: "flex-start",
-		alignContent: "flex-start",
-		justifyContent: "flex-start"
-	}
+  Palette: {
+    height: "93vh",
+    overflow: "visible",
+    marginTop: "7vh",
+    marginLeft: "15vw",
+    display: "flex",
+    flexFlow: "row wrap",
+    alignItems: "flex-start",
+    alignContent: "flex-start",
+    justifyContent: "flex-start",
+  },
 };
